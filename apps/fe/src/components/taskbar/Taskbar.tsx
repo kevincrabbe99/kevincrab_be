4137eecf62885e9612cb48a379bccac4f52722dd
Kevin Crabbe <kevincrabbe99@gmail.com>
2022-10-17 19:31:42 -0700

    minor taskbar improvements

--- apps/fe/src/components/taskbar/Taskbar.tsx
+++ apps/fe/src/components/taskbar/Taskbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { WindowConfig, WindowState } from '../../reducers/windowReducer';
 import "./taskbar.scss"
@@ -16,6 +16,12 @@ export default function Taskbar( props: any ) {
     
     const windowState: WindowState = useSelector((state: any) => state.windows);
 
+    const [taskBarItemsStartIndex, setTaskBarItemsStartIndex] = useState<number>(0)
+    const [taskBarItemCount, setTaskBarItemCount] = useState<number>(0)
+    const taskBarItemWidth = 150
+    const viewPortWindowWidth = (window.innerWidth * 0.8) - 80 - 70
+    const taskBarItemCapacity = viewPortWindowWidth / taskBarItemWidth
+
     const selectTaskbarPillEvent = (id: string): void => {
         dispatch({type: "FOCUS_WINDOW", payload: id})
     }
@@ -25,6 +31,25 @@ export default function Taskbar( props: any ) {
         if (window.minimized) { return false }
         return false
     }
+
+    // used to keep track of the taskbar items count
+    useEffect(() => {
+        // count the number of taskbar items
+        let count = 0
+        let lastSelectedIndex = 0
+        for (let i = 0; i < windowState.windows.length; i++) {
+            const currentWindow = windowState.windows[i]
+            if (!currentWindow.exited) { count++ }
+            if (currentWindow.id === windowState.top) { lastSelectedIndex = i }
+        }
+        setTaskBarItemCount(count)
+        setTaskBarItemsStartIndex(0)
+        
+        console.log("viewport width: " + viewPortWindowWidth)
+        console.log("taskbar item count: " + taskBarItemCount)
+        console.log("taskbar start index: ", taskBarItemsStartIndex)
+        console.log("taskbar item capacity: " + taskBarItemCapacity)
+    }, [windowState])
 
     return (
     <div className="wrapper-taskbar">
@@ -39,9 +64,23 @@ export default function Taskbar( props: any ) {
                     </td>
                     
                     {
-                        windowState.windows.map((window: any) => {
+                        taskBarItemsStartIndex > 0 &&
+                        <td>
+                            <div className="taskbar-moveLeft tb-move-button" onClick={() => setTaskBarItemsStartIndex(taskBarItemsStartIndex - 1)}>
+                             <label>
+                              &#9664;
+                             </label>
+                            </div>
+                        </td>
+                    }
+                    
+                    
+                    {
+                        windowState.windows.map((window: any, index: number) => {
                             return (
-                                window.exited != true ?
+                                window.exited != true &&
+                                index >= taskBarItemsStartIndex && 
+                                index < taskBarItemsStartIndex + taskBarItemCapacity ?
                                 <td>
                                     <div    className={`application_placeholder-wrapper ${!window.exited && !window.minimized ? 'tb-selected' : ''}`}
                                             onClick={() => selectTaskbarPillEvent(window.id)}>
@@ -53,19 +92,20 @@ export default function Taskbar( props: any ) {
                             )
                         })
                     }
-
-                    {/* <td>
-                        <div className="application_placeholder-wrapper">
-                            <img src="./icons/internet.ico"></img>
-                            <label>Internet</label>
-                        </div>
-                    </td> */}
                 </tr>
             </table>
         </div>
         <div className="taskbar-right-box">
             <label>{ time }</label> 
         </div>
+        {
+            (taskBarItemCount - taskBarItemsStartIndex) > taskBarItemCapacity &&
+            <div className="taskbar-moveRight tb-move-button" onClick={() => setTaskBarItemsStartIndex(taskBarItemsStartIndex + 1)}>
+                <label>
+                &#9654;
+                </label>
+            </div>
+        }
     </div>
     )
 }
