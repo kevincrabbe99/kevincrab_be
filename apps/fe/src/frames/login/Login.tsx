b4477e89e2d34cecc9f3fbd2127bdcd692ffb9f9
Kevin Crabbe <kevincrabbe99@gmail.com>
2022-10-18 20:37:55 -0700

    better window positioning for keyboard view

--- apps/fe/src/frames/login/Login.tsx
+++ apps/fe/src/frames/login/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { CSSProperties, useEffect, useRef, useState } from 'react'
 import Window from '../../components/window/Window'
 import { WindowConfig } from '../../reducers/windowReducer';
 import "./login.scss"
@@ -25,12 +25,44 @@ const loginWindow: WindowConfig = {
 
 export default function Login() {
 
+    // listen for document.documentElement.clientWidth changes
+    const [windowHeight, setWindowHeight] = useState(document.documentElement.clientHeight)
+    const [nudgerStyle, setNudgerStyle] = useState<CSSProperties>({top: 0})
+    const initialWindowHeight = useRef(document.documentElement.clientHeight)
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowHeight(document.documentElement.clientHeight)
+        }
+        window.addEventListener('resize', handleResize)
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+
+
+    // used to nudge window height when keyboard is open
+    useEffect(() => {
+        // calculate difference
+        let difference = windowHeight - initialWindowHeight.current
+
+        // select child class of nudgerStyle .window-wrapper
+        let windowWrappers = document.querySelectorAll(".window-wrapper")
+        // loop through windowWrappers
+        for (let i = 0; i < windowWrappers.length; i++) {
+            // set windowWrappers[i].style.top to difference on type element
+            (windowWrappers[i] as HTMLElement).style.marginTop = (difference / 2) + "px"
+        }
+
+
+    }, [windowHeight])
+
     return (
         <div className="login-wrapper">
             <div className="login-background-wrapper">
                 <img src="./images/clouds.jpeg"></img>
             </div>
             <div className="login-page-content">
+
                 <Window windowConfig={loginWindow} />
             </div>
         </div>
