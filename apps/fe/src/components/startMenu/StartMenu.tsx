d95df8c115ce3f944296cca4c8c69bfdb1319277
Kevin Crabbe <kevincrabbe99@gmail.com>
2022-10-18 18:38:18 -0700

    start menu improvements

--- apps/fe/src/components/startMenu/StartMenu.tsx
+++ apps/fe/src/components/startMenu/StartMenu.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { StartMenuItem } from '../../types/StartMenuItem'
+import { StartMenuActionType, StartMenuItem } from '../../types/StartMenuItem'
 import "./startMenu.scss"
 
 import startMenuJson from '../../assets/json/start_menu.json'
@@ -7,25 +7,41 @@ import { DestinationActionTriggers } from '../../types/DestinationActionTriggers
 import { useDispatch } from 'react-redux'
 import { FrameStatesEnum } from '../../reducers/frameReducer'
 import { fromEvent, map } from 'rxjs'
+import { browserWindowConfig } from '../windowPages/browser/BrowserPage'
+import { folderWindowConfig } from '../windowPages/folder/FolderPage'
 
 
 
-export default function StartMenu() {
+export default function StartMenu(props: any) {
+
+  const setIsStartMenuOpen = props.setIsStartMenuOpen
 
   const [selectedSubmenu, setSelectedSubmenu] = useState<string | null>(null)
 
 
   const dispatch = useDispatch();
 
-  const handleDestinationAction = (destination: DestinationActionTriggers) => {
-      switch(destination) {
-          case DestinationActionTriggers.SHUTDOWN:
-              console.log("shutdown")
-              dispatch({type: "SET_STATE", payload: FrameStatesEnum.SHUTDOWN});
-              break;
-          default:
-               break;
+  const handleDestinationAction = (action: StartMenuActionType) => {
+      switch(action.destination) {
+            case DestinationActionTriggers.SHUTDOWN:
+                console.log("shutdown")
+                dispatch({type: "SET_STATE", payload: FrameStatesEnum.SHUTDOWN});
+                break;
+            case DestinationActionTriggers.OPEN_BROWSER:
+                console.log("open browser")
+                browserWindowConfig.contentData = action.param;
+                dispatch({type: "ADD_WINDOW", payload: browserWindowConfig});
+                break;
+            case DestinationActionTriggers.OPEN_FOLDER:
+                console.log("open folder")
+                folderWindowConfig.contentData = action.param;
+                dispatch({type: "ADD_WINDOW", payload: folderWindowConfig});
+                break;
+            default:
+                break;
       }
+
+      setIsStartMenuOpen(false)
   }
 
   return (
@@ -84,7 +100,7 @@ const renderStartMenuItemActionWrapper = (item: StartMenuItem, setSelectedSubmen
     if (item.action.isExternal === 0 && !item.submenu) {
         return (
             <li>
-                <div className="startMenu-list-item" onClick={() => handleDestinationAction(item.action.destination as DestinationActionTriggers)}>
+                <div className="startMenu-list-item" onClick={() => handleDestinationAction(item.action)}>
                     {renderStartMenuItem(item, setSelectedSubmenu, selectedSubmenu)}
                 </div>
                 {
