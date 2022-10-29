5b3bb83e5ff1a77b23c379d056d8378d47b130ee
Kevin Crabbe <kevincrabbe99@gmail.com>
2022-10-28 23:24:42 -0700

    remove env check for firebase init

--- apps/fe/src/index.tsx
+++ apps/fe/src/index.tsx
@@ -15,21 +15,20 @@ import { getAnalytics } from 'firebase/analytics';
 var firebaseApp;
 var ga4;
 
-if (process.env.REACT_APP_NODE_ENV === 'production' || 
-    process.env.REACT_APP_NODE_ENV === 'development') {
-  const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-    appId: process.env.REACT_APP_FIREBASE_APPID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
-  };
-  
-  firebaseApp = initializeApp(firebaseConfig);
-  ga4 = getAnalytics(firebaseApp);
-}
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+};
+
+firebaseApp = initializeApp(firebaseConfig);
+ga4 = getAnalytics(firebaseApp);
+
 
 
 
