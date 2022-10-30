af19db899c1e16fabbbc1dc6ab31f61ac7c95c53
Kevin Crabbe <kevincrabbe99@gmail.com>
2022-10-29 17:06:58 -0700

    ga4 work

--- apps/fe/src/index.tsx
+++ apps/fe/src/index.tsx
@@ -10,7 +10,62 @@ import { store } from './store';
 
 
 
+import firebase, { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
+
+var firebaseApp;
+var ga4;
+
+
+
+var firebaseConfig
+
+console.log("process.env.NODE_ENV", process.env.NODE_ENV)
+console.log("process.env.REACT_APP_NODE_ENV", process.env.REACT_APP_NODE_ENV)
+
+console.log("process.env.REACT_APP_FIREBASE_API_KEY", process.env.REACT_APP_FIREBASE_APIKEY)
+console.log("process.env.REACT_APP_FIREBASE_AUTH_DOMAIN", process.env.REACT_APP_FIREBASE_AUTHDOMAIN)
+console.log("process.env.REACT_APP_FIREBASE_PROJECT_ID", process.env.REACT_APP_FIREBASE_PROJECTID)
+console.log("process.env.REACT_APP_FIREBASE_STORAGE_BUCKET", process.env.REACT_APP_FIREBASE_STORAGEBUCKET)
+console.log("process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID", process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID)
+console.log("process.env.REACT_APP_FIREBASE_APP_ID", process.env.REACT_APP_FIREBASE_APPID)
+console.log("process.env.REACT_APP_FIREBASE_MEASUREMENT_ID", process.env.REACT_APP_FIREBASE_MEASUREMENTID)
+
+console.log("process.env.FIREBASE_APIKEY", process.env.FIREBASE_APIKEY)
+console.log("process.env.FIREBASE_AUTHDOMAIN", process.env.FIREBASE_AUTHDOMAIN)
+console.log("process.env.FIREBASE_PROJECTID", process.env.FIREBASE_PROJECTID)
+console.log("process.env.FIREBASE_STORAGEBUCKET", process.env.FIREBASE_STORAGEBUCKET)
+console.log("process.env.FIREBASE_MESSAGINGSENDERID", process.env.FIREBASE_MESSAGINGSENDERID)
+console.log("process.env.FIREBASE_APPID", process.env.FIREBASE_APPID)
+console.log("process.env.FIREBASE_MEASUREMENTID", process.env.FIREBASE_MEASUREMENTID)
+
+
+if (process.env.NODE_ENV === 'development') {
+  firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+  };
+  firebaseApp = initializeApp(firebaseConfig);
+  ga4 = getAnalytics(firebaseApp);
+} else if (process.env.NODE_ENV === 'production') {
+  firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+  };
+  firebaseApp = initializeApp(firebaseConfig);
+  ga4 = getAnalytics(firebaseApp);
+}
 
 
 
