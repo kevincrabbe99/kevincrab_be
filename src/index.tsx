c615029c164414b8addc7e37bbd5892a13536a75
Computa <computa@Computas-Mac-mini.local>
2026-02-13 19:04:50 -0800

    Fix SCSS autoprefixer warnings
    - Load Bootstrap CSS from CDN instead of npm package
    - Remove bootstrap CSS import from index.tsx
    - Replace justify-content: end with flex-end in 3 SCSS files:
      - defaultSettingsTable.scss
      - genericModalPage.scss
      - run.scss

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
